99a38709d969f70daf89a46c51739995967b7522
plowzzer <poloo.p3@gmail.com>
2022-03-22 23:15:54 -0300

    code formater and new page style

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,37 @@
 import type { NextPage } from "next";
 import { useState } from "react";
 
-import simulate from '../utils/Simulation'
+import simulate from "../utils/Simulation";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
+  const [loading, setLoading] = useState(false);
   const [rightWord, setRightWord] = useState("");
   const [rightWordError, setRightWordError] = useState("");
   const [lettersNot, setLettersNot] = useState("");
   const [posibilities, setPossibilities] = useState([] as string[]);
 
-  const handleSimulate = async () => {
-    setRightWordError("")
+  const handleSimulateForm = async (e: any) => {
+    e.preventDefault();
 
-    if (rightWord.length !== 5) {
-      setRightWordError("Você deve adicionar uma palavra de 5 letras")
-      return
+    try {
+      setLoading(true);
+
+      setRightWordError("");
+
+      if (rightWord.length !== 5) {
+        setRightWordError("Você deve adicionar uma palavra de 5 letras");
+        return;
+      }
+
+      const response: string[] = await simulate(rightWord, lettersNot);
+      setPossibilities(response);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
     }
-
-    const response:string[] = await simulate(rightWord, lettersNot)
-    setPossibilities(response)
   };
 
   return (
@@ -33,71 +44,75 @@ const Home: NextPage = () => {
 
       <main className="container mx-auto px-4 py-16">
         <h1 className={styles.title}>
-          Welcome to <a href="https://term.ooo">Termooo</a> Helper
+          <a href="https://term.ooo">Termooo</a> Helper
         </h1>
         <div className={styles.version}>
-          <small>v1.0.2</small>
-        </div>
-        <p className={styles.description}>
-          Entre com a palavra <code className={styles.code}>TerMo</code> e
-          retornamos com a sujestão
-        </p>
-
-        <div>
-          <label htmlFor="word" className="font-semibold">
-            Palavra
-          </label>
-          <input
-            id="word"
-            className={`placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md px-4 py-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm ${rightWordError && "border-red-500"}`}
-            placeholder="<L> verdes <l> amarelos <_> escuro"
-            onChange={(e) => {
-              setRightWord(e.target.value);
-            }}
-            maxLength={5}
-            minLength={5}
-            type="text"
-            name="word"
-          />
-          {rightWordError && <p className="text-red-600">{rightWordError}</p>}
+          <small>v1.0.3</small>
         </div>
 
-        <div className="mt-6">
-          <label htmlFor="not-in" className="font-semibold">
-            Letras que não existem
-          </label>
-          <input
-            id="not-in"
-            className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md px-4 py-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
-            placeholder="digite as letras que não existem"
-            onChange={(e) => {
-              setLettersNot(e.target.value);
-            }}
-            type="text"
-            name="not-in"
-          />
-        </div>
+        <form onSubmit={handleSimulateForm}>
+          <div className="mx-auto w-96 content-center grid">
+            <label htmlFor="word" className={styles.label}>
+              Palavra
+              <small>{` <A> verdes <a> amarelos <_> escuro`}</small>
+            </label>
+            <input
+              id="word"
+              className={`${styles.input} ${styles.inputRight}`}
+              onChange={(e) => {
+                setRightWord(e.target.value);
+              }}
+              maxLength={5}
+              minLength={5}
+              type="text"
+              name="word"
+            />
+            {rightWordError && <p className="text-red-600">{rightWordError}</p>}
+          </div>
 
-        <div className="mt-6">
-          <button
-            type="button"
-            className="bg-indigo-600 text-white font-medium py-3 px-4 rounded-md"
-            onClick={handleSimulate}
-          >
-            Simular
-          </button>
-        </div>
-        
-        {posibilities && (
-          <div>
-            <p className="text-center">Foram encontradas {posibilities.length} palavras</p>
-            <ul className="flex flex-wrap">
+          <div className="mt-6 mx-auto w-96 content-center grid">
+            <label htmlFor="not-in" className={styles.label}>
+              Letras
+              <small>{` digite as letras que não existem`}</small>
+            </label>
+            <input
+              id="not-in"
+              className={`${styles.input} ${styles.inputNon}`}
+              onChange={(e) => {
+                setLettersNot(e.target.value);
+              }}
+              type="text"
+              name="not-in"
+            />
+          </div>
+
+          <div className="mt-6 mx-auto w-96 content-center grid">
+            <input
+              type="submit"
+              className={styles.button}
+              value={loading ? "Carregando..." : "Simular"}
+              disabled={loading}
+            />
+          </div>
+        </form>
+
+        {posibilities.length > 0 && (
+          <div className="mt-10">
+            <p className="text-center mb-4">
+              Foram encontradas {posibilities.length} possibilidades
+            </p>
+            <ul className={styles.results}>
               {posibilities.map((posibility, index) => (
-                <li key={`${posibility}-${index}`} className="flex m-2 py-3 px-4 text-white bg-blue-400 rounded-lg">
+                <li key={`${posibility}-${index}`} className={styles.result}>
                   {posibility}
                 </li>
               ))}
             </ul>
+            <p className="text-center mt-10">
+              <small>
+                Obs.: Foi removido a acentuação de todas as palavras
+              </small>
+            </p>
           </div>
         )}
       </main>
--- utils/Simulation.ts
+++ utils/Simulation.ts
@@ -1,80 +1,79 @@
 type Data = {
-  simulation: string[]
-}
+  simulation: string[];
+};
 
-export default function handler(rightWord:string, lettersNot:string) {
-  return findWords(rightWord, lettersNot)
+export default function handler(rightWord: string, lettersNot: string) {
+  return findWords(rightWord, lettersNot);
 }
 
 async function getDictionary() {
-  const dataFromDocument = await fetch('fiveWordList.txt')
-  const wordFormat = await dataFromDocument.text()
-  return wordFormat.split('\n')
+  const dataFromDocument = await fetch("fiveWordList.txt");
+  const wordFormat = await dataFromDocument.text();
+  return wordFormat.split("\n");
 }
 
+async function findWords(word: string, lettersNotIn: string) {
+  const dictonary = await getDictionary();
+  const wordArray = word.split("");
+  const wordsThatFit: string[] = [];
+  const arrayOfNotInRightPlace: string[] = [];
+  let haveRights = false;
+  let haveNotInRightPlace = false;
+  let wordRightCount = 0;
 
-async function findWords(word:string, lettersNotIn:string) {
-  const dictonary = await getDictionary()
-  const wordArray = word.split('')
-  const wordsThatFit:string[] = []
-  const arrayOfNotInRightPlace:string[] = []
-  let haveRights = false
-  let haveNotInRightPlace = false
-  let wordRightCount = 0
-
-  wordArray.forEach(letter => {
-    if (letter === letter.toUpperCase() && letter !== '_') {
-      haveRights = true
-      wordRightCount++
+  wordArray.forEach((letter) => {
+    if (letter === letter.toUpperCase() && letter !== "_") {
+      haveRights = true;
+      wordRightCount++;
     }
-    if (letter === letter.toLowerCase() && letter !== '_') {
-      haveNotInRightPlace = true
-      arrayOfNotInRightPlace.push(letter)
+    if (letter === letter.toLowerCase() && letter !== "_") {
+      haveNotInRightPlace = true;
+      arrayOfNotInRightPlace.push(letter);
     }
-  })
+  });
 
   if (haveRights) {
-    dictonary.filter(dictionaryWord => {
-      const dictionaryWordArray = dictionaryWord.toUpperCase().split('')
-      let lettersGreen = 0
+    dictonary.filter((dictionaryWord) => {
+      const dictionaryWordArray = dictionaryWord.toUpperCase().split("");
+      let lettersGreen = 0;
       dictionaryWordArray.forEach((letter, index) => {
         if (letter === wordArray[index]) {
-          lettersGreen++
+          lettersGreen++;
         }
-      })
+      });
 
       if (lettersGreen === wordRightCount) {
-        wordsThatFit.push(dictionaryWord)
+        wordsThatFit.push(dictionaryWord);
       }
-    })
+    });
   }
-  
+
   if (arrayOfNotInRightPlace) {
-    for (let index = wordsThatFit.length-1 ; index >= 0; index--) {
+    for (let index = wordsThatFit.length - 1; index >= 0; index--) {
       const word = wordsThatFit[index];
       for (let j = 0; j < arrayOfNotInRightPlace.length; j++) {
-        const letter = arrayOfNotInRightPlace[j]
+        const letter = arrayOfNotInRightPlace[j];
         if (!word.toLowerCase().includes(letter)) {
-          wordsThatFit.splice(index, 1)
-          break
+          wordsThatFit.splice(index, 1);
+          break;
         }
       }
     }
   }
 
   if (wordsThatFit.length > 1) {
-    for (let index = wordsThatFit.length-1 ; index >= 0; index--) {
+    for (let index = wordsThatFit.length - 1; index >= 0; index--) {
       const word = wordsThatFit[index];
-      const lettersNotInArray = lettersNotIn.split('')
+      const lettersNotInArray = lettersNotIn.split("");
       for (let j = 0; j < lettersNotInArray.length; j++) {
-        const letter = lettersNotInArray[j]
+        const letter = lettersNotInArray[j];
         if (word.includes(letter)) {
-          wordsThatFit.splice(index, 1)
-          break
+          wordsThatFit.splice(index, 1);
+          break;
         }
       }
     }
   }
 
-  return wordsThatFit
+  return wordsThatFit;
 }
